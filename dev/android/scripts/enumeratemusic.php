a04e3624c9765c6af9f9458195eefb308dbdc59f
Travis Reynolds <travis97@outlook.com>
2016-11-14 14:13:35 +0100

    scripts

---
+++ dev/android/scripts/enumeratemusic.php
@@ -0,0 +1,29 @@
+<?php
+/*Make connection database*/
+/*---------------------------------username----------------db_name*/
+$con = mysqli_connect("localhost", "achaado", "wp9sqxj97", "music");
+
+/*check connection*/
+if (mysqli_connect_errno()) {
+  printf("Connection Failed: %s\n", mysqli_connect_error());
+  exit();
+  # code...
+}
+
+/*scan directory for files*/
+$files = glob('*.mp3');
+
+usort ($files, function ($a, $b) {
+  return filemtime($a) < filemtime($b);
+});
+
+/*insert list of files to database IF they dont exist already*/
+$i = 0;
+while ($files[$i]); {
+   $trackname = basename($files[$i]);
+   echo $trackname."**";
+   $i++;
+}
+
+
+ ?>
