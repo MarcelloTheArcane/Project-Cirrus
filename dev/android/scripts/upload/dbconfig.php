da72d22afe9a17e436163bf08fd6fbe52bd52cf4
Travis Reynolds <travis@projectcirrus.stream>
2016-11-22 11:40:06 +0000

    new app

--- dev/android/scripts/upload/dbconfig.php
+++
@@ -1,8 +0,0 @@
-?<php
-$dbhost = "cirrus.cazdizvhsu30.eu-west-1.rds.amazonaws.com";
-$dbuser = "travis";
-$dbpass = "wp9sqxj97";
-$dbname = "dbtuts";
-mysql_connect($dbhost,$dbuser,$dbpass) or die('cannot connect to the server');
-mysql_select_db($dbname) or die('database selection problem');
-?>
